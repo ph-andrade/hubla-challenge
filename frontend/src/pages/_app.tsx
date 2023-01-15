2f01c721e27cfe31daa8b3b5382eceb3b5911b60
Paulo Andrade <paulo.gash@hotmail.com>
2023-01-15 19:51:11 -0300

    Feature: init style compoments and theme settings

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import GlobalStyle from '../styles/global'
+import theme from '../styles/theme'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
+
+export default MyApp
