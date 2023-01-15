5d25dc924b34b9d947ad6a6567a4f79e0093b69a
Paulo Andrade <paulo.gash@hotmail.com>
2023-01-15 12:49:23 -0300

    Feature: save database sellers and transactions using repository implementations

--- backend/src/modules/transaction/transaction.module.ts
+++ backend/src/modules/transaction/transaction.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
+import { PrismaTransactionsRepository } from 'src/repositories/prisma/PrismaTransactionsRepository';
+import { TransactionsRepository } from 'src/repositories/TransactionsRepository';
+import { SellersRepository } from 'src/repositories/SellersRepository';
+import { PrismaSellersRepository } from 'src/repositories/prisma/PrismaSellersRepository';
 
 @Module({
   controllers: [TransactionController],
-  providers: [TransactionService],
+  providers: [
+    PrismaTransactionsRepository,
+    PrismaSellersRepository,
+    {
+      provide: TransactionService,
+      useFactory: (
+        transactionsRepository: TransactionsRepository,
+        sellersRepository: SellersRepository,
+      ) => {
+        return new TransactionService(
+          transactionsRepository,
+          sellersRepository,
+        );
+      },
+      inject: [PrismaTransactionsRepository, PrismaSellersRepository],
+    },
+  ],
 })
 export class TransactionModule {}
--- backend/src/modules/transaction/transaction.service.ts
+++ backend/src/modules/transaction/transaction.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { validatorDto } from 'src/utils/ValidatorDTO';
+import { TransactionsRepository } from 'src/repositories/TransactionsRepository';
 import { TransactionDTO } from './transaction.dto';
+import { validatorDto } from 'src/utils/ValidatorDTO';
+import { SellersRepository } from 'src/repositories/SellersRepository';
 
 @Injectable()
 export class TransactionService {
+  constructor(
+    private transactionsRepository: TransactionsRepository,
+    private sellersRepository: SellersRepository,
+  ) {}
+
   async import(file: Express.Multer.File) {
     const fileData = file.buffer.toString().split('\n');
     if (!fileData.length) throw new Error(`There's no data to import`);
+    const totalRows = fileData.length;
+    const failedRows: number[] = [];
+    const validatedTransactions: TransactionDTO[] = [];
 
     // Standardize and validate data
-    const validatedTransactions: TransactionDTO[] = [];
     await Promise.all(
-      fileData.map(async (data) => {
+      fileData.map(async (data, index) => {
         const transaction = {
           type: Number(data.slice(0, 1)),
           date: new Date(data.slice(1, 26)),
@@ -23,9 +32,42 @@ export class TransactionService {
         if (await validatorDto(TransactionDTO, transaction)) {
           return validatedTransactions.push(transaction);
         }
+        failedRows.push(index);
       }),
     );
 
-    return validatedTransactions;
+    // Build an object that has the seller's name as a key and his transactions as an array
+    const sellerTransactions = validatedTransactions.reduce(
+      (result: { [sellerName: string]: TransactionDTO[] }, current) => {
+        const { type, date, product, value, sellerName } = current;
+        const key = sellerName;
+        result[key] = result[key] || [];
+        result[key].push({ type, date, product, value });
+        return result;
+      },
+      {},
+    );
+
+    // checks if the seller already exists, if not create it and thieir the transactions
+    await Promise.all(
+      Object.keys(sellerTransactions).map(async (sellerName) => {
+        let seller = await this.sellersRepository.findByName(sellerName);
+        if (!seller) seller = await this.sellersRepository.create(sellerName);
+
+        return this.transactionsRepository.bulkCreate(
+          sellerTransactions[sellerName].map((sellerTransaction) => {
+            return {
+              ...sellerTransaction,
+              sellerId: seller.id,
+            };
+          }),
+        );
+      }),
+    );
+
+    return {
+      totalRows,
+      failedRows,
+    };
   }
 }
