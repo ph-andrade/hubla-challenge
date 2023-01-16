7004989ad9f5b42d906cc733ccce2cb0c8f15afa
Paulo Andrade <paulo.gash@hotmail.com>
2023-01-16 20:17:54 -0300

    Feature: Changed Home page and remove useSellers hook

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,27 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
+import {
+  AiOutlineLoading3Quarters,
+} from 'react-icons/ai';
 
-import { Container, TableContainer } from '../styles/pages/Home'
+import { Layout } from '../styles/components/Layout'
 import Header from '@/components/Header'
 import SellerTable from '@/components/SellerTable'
 import TransactionTable from '@/components/TransactionTable'
-import { SellersProvider } from '@/hooks/useSellers'
+import { Seller } from '@/interfaces/Seller'
+import ExportFile from '@/components/ExportFile'
+import { Loading } from '@/styles/components/Loading';
+import api from '@/services/api';
 
 const Home: React.FC = () => {
+  const [sellers, setSellers] = useState<Seller[]>([]);
+  const [selectedSeller, setSelectedSeller] = useState<Seller>();
+  const [page, setPage] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  async function loadSellers(currentPage: number) {
+    setLoading(true);
+    const response = await api.get('/seller', { params: { page: currentPage } });
+
+    if(response.status === 200) {
+      setSellers(response.data);
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadSellers(page);
+  }, [page]);
+
   return (
     <>
       <Header />
-      <Container>
-        <Head>
-          <title>Hubla</title>
-        </Head>
-        <SellersProvider>
-          <TableContainer>
-            <SellerTable />
-            <TransactionTable />
-          </TableContainer>
-        </SellersProvider>
-      </Container>
+      <Head>
+        <title>Hubla</title>
+      </Head>
+      {loading ? (
+        <Loading>
+          <AiOutlineLoading3Quarters size={200} />
+        </Loading>
+      ) : (
+        <Layout>
+          {
+            selectedSeller ?
+            (<>
+              <TransactionTable 
+                selectedSeller={selectedSeller}
+                setSelectedSeller={setSelectedSeller}
+              />  
+            </>) :
+            (<>
+              <SellerTable 
+                sellers={sellers}
+                setSelectedSeller={setSelectedSeller}
+                page={page}
+                setPage={setPage}
+                setLoading={setLoading}
+              />
+              <ExportFile loadSellers={loadSellers}/>
+            </>)
+          } 
+        </Layout>
+      )}
     </>
   )
 }
