0e7f92873e74342f5879df2d33e44adf21a466e7
Paulo Andrade <paulo.gash@hotmail.com>
2023-01-17 00:26:51 -0300

    Feature: Added unit tests on backend

--- backend/src/modules/seller/seller.service.spec.ts
+++ backend/src/modules/seller/seller.service.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { SellersRepositoryInMemory } from '../../repositories/in-memory/SellersRepositoryInMemory';
+import { SellersRepository } from '../../repositories/SellersRepository';
 import { SellerService } from './seller.service';
 
 describe('SellerService', () => {
   let service: SellerService;
+  let sellersRepository: SellersRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SellerService],
+      providers: [
+        SellersRepositoryInMemory,
+        {
+          provide: SellerService,
+          useFactory: (sellersRepository: SellersRepository) => {
+            return new SellerService(sellersRepository);
+          },
+          inject: [SellersRepositoryInMemory],
+        },
+      ],
     }).compile();
 
     service = module.get<SellerService>(SellerService);
+    sellersRepository = module.get<SellersRepositoryInMemory>(
+      SellersRepositoryInMemory,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be able to list the sellers', async () => {
+    const name = 'Name Test';
+    await sellersRepository.create(name);
+    const sellers = await service.list({ page: 0, limit: 1 });
+
+    expect(sellers).toEqual(
+      expect.arrayContaining([expect.objectContaining({ name })]),
+    );
+  });
+
+  it('should be able to paginate the sellers', async () => {
+    const name1 = 'Name Test';
+    const name2 = 'Name Test 2';
+    await sellersRepository.create(name1);
+    await sellersRepository.create(name2);
+
+    const sellers1 = await service.list({ page: 0, limit: 1 });
+    expect(sellers1).toEqual(
+      expect.arrayContaining([expect.objectContaining({ name: name1 })]),
+    );
+    const sellers2 = await service.list({ page: 1, limit: 1 });
+    expect(sellers2).toEqual(
+      expect.arrayContaining([expect.objectContaining({ name: name2 })]),
+    );
   });
 });
--- backend/src/modules/transaction/transaction.service.spec.ts
+++ backend/src/modules/transaction/transaction.service.spec.ts
@@ -1,18 +1,67 @@
+import { resolve } from 'path';
+import * as fs from 'fs';
+
 import { Test, TestingModule } from '@nestjs/testing';
+import { SellersRepositoryInMemory } from '../../repositories/in-memory/SellersRepositoryInMemory';
+import { TransactionsRepositoryInMemory } from '../../repositories/in-memory/TransactionsRepositoryInMemory';
+import { SellersRepository } from '../../repositories/SellersRepository';
+import { TransactionsRepository } from '../../repositories/TransactionsRepository';
 import { TransactionService } from './transaction.service';
 
 describe('TransactionService', () => {
   let service: TransactionService;
+  let sellersRepository: SellersRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionService],
+      providers: [
+        TransactionsRepositoryInMemory,
+        SellersRepositoryInMemory,
+        {
+          provide: TransactionService,
+          useFactory: (
+            transactionsRepository: TransactionsRepository,
+            sellersRepository: SellersRepository,
+          ) => {
+            return new TransactionService(
+              transactionsRepository,
+              sellersRepository,
+            );
+          },
+          inject: [TransactionsRepositoryInMemory, SellersRepositoryInMemory],
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionService>(TransactionService);
+    sellersRepository = module.get<SellersRepositoryInMemory>(
+      SellersRepositoryInMemory,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be able to import files', async () => {
+    const testFolder = resolve(__dirname, '..', '..', '..', 'test');
+    const filename = resolve(`${testFolder}`, 'sales.txt');
+
+    const seller = {
+      name: 'JOSE CARLOS',
+    };
+
+    // const fileData = file.arrayBuffer.toString().split('\n');
+    const fileDate = await fs.promises.readFile(filename, 'utf-8');
+    const response = await service.import(fileDate);
+    expect(response).toEqual({
+      totalRows: 2,
+      failedRows: [1],
+    });
+
+    const sellers = await sellersRepository.list({ page: 0, limit: 1 });
+    expect(sellers).toEqual(
+      expect.arrayContaining([expect.objectContaining(seller)]),
+    );
   });
 });
